9583fa22e3240652d504c57f2e1b76ffa031e07b
Squirrel <squirrelhomebrew@gmail.com>
2025-06-11 01:58:31 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,35 @@
 import { resolve } from 'path'
+import { defineConfig } from 'vite'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
-export default {
+export default defineConfig({
   root: resolve(__dirname, 'src'),
   build: {
-    outDir: '../dist'
+    outDir: '../dist',
   },
   server: {
-    port: 8080
+    port: 8080,
   },
   css: {
-     preprocessorOptions: {
-        scss: {
-          silenceDeprecations: [
-            'import',
-            'mixed-decls',
-            'color-functions',
-            'global-builtin',
-          ],
-        },
-     },
+    preprocessorOptions: {
+      scss: {
+        silenceDeprecations: [
+          'import',
+          'mixed-decls',
+          'color-functions',
+          'global-builtin',
+        ],
+      },
+    },
   },
-}
+  plugins: [
+    viteStaticCopy({
+      targets: [
+        {
+          src: resolve(__dirname, 'src/assets/badges'),
+          dest: 'assets',
+        },
+      ],
+    }),
+  ],
+})
